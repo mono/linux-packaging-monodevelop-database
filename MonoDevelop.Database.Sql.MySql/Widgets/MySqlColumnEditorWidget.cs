77add56d045fef720c71fba46604a07b562d0bd3
Jo Shields <directhex@apebox.org>
2010-04-25 18:03:11 +0100

    Imported Upstream version 2.2+dfsg

---
+++ MonoDevelop.Database.Sql.MySql/Widgets/MySqlColumnEditorWidget.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace MonoDevelop.Database.Sql.MySql
+{
+	
+	
+	[System.ComponentModel.Category("MonoDevelop.Database.Sql.MySql")]
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class MySqlColumnEditorWidget : Gtk.Bin
+	{
+		
+		public MySqlColumnEditorWidget()
+		{
+			this.Build();
+		}
+	}
+}
